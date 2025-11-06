20be23f2d770236b73f953fba2088896ea0501fe
alkapandit <alkapandit6101998@gmail.com>
2025-11-07 00:03:09 +0530

    feat: database connected successfully

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+
+    // Connect to MongoDB
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    return conn;
+
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1); // Exit app on DB connection failure
+  }
+};
+
+export default connectDB;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import app from "./app.js";
 import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`✅ Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => { console.log("MONGO db connection failed !!! ", err); });
 
